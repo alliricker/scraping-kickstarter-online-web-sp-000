6d536d185f28efaea5cf21931290e7828e8f5d60
Allison Ricker <alliricker@gmail.com>
2020-10-20 01:19:55 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,9 +2,11 @@ require "nokogiri"
 require "pry"
 
 def create_project_hash
+  # projects: kickstarter.css("li.project.grid_4")q
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   binding.pry
 end
 
 create_project_hash
+
